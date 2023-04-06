85ce2f605205eb8f499900b101520537205612dd
britthubs <britt.van.schoors@gmail.com>
2023-04-06 17:51:02 +0200

    Changed to dnd stats, add method 3d6

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 # import
 import random as ran
-import time as t
 from termcolor import colored
 import os
 
-# set up list
+# set up lists & string (for the summary at the end)
 names = []
-stats = []
+stats_tot = []
+stats_string = ""
 
-# 6 and 8-sided dice
-def six_eight():
+# method list + input method
+def method():
+    print("Method Menu:")
+    print("*" * 11)
+    print("1: 3d6 \n")
+    methodNum = input("Type the number associated with your preferred method to roll for your character stats: ")
+    while methodNum != "1":
+        methodNum = input("Please only write the number associated with your preferred method: ")
+    if methodNum == "1":
+        three_dsix()  # go to the function for method 3d6
+
+# 3d6 per stat
+def three_dsix():
     charac = input("What's you character's name?: ")
-    six = ran.randint(1, 6)
-    eight = ran.randint(1, 8)
-    health = six * eight
-    names.append(charac)
-    stats.append(health)
-    print("Their health is:", health, "hp")
+    names.append(charac)  # add the name of the character to a list for the summary at the end
+    stats = []  # makes sure this list is empty again before adding stats to it
+    for i in range(6):
+        stat = ran.randint(1,6) + ran.randint(1,6) + ran.randint(1,6) # 3d6 for one stat
+        stats.append(stat)  # at the stat to a list for this character's stats
+    stats_tot.append(stats)  # put the list of the stats for this character in a list for the summary at the end
+
 
 # print title
 print("🎲 Character Stats Generator 🎲")
 print("*" * 31)
-six_eight()
+method()  # shows the method menu
+
 
 # ask for repeat or not
 again = "y"
@@ -30,7 +43,8 @@ while again == "y":
     while again != "y" and again != "n":
         again = input("Only write y for 'yes' and n for 'no' \nRoll for new character? \ny or n: ")
     if again == "y":
-        six_eight()
+        method()  # shows the method menu
+
 
 os.system("clear")
 print("These are your characters and their stats:")
@@ -38,10 +52,15 @@ print("*" * 42, "\n")
 
 # print a summary of names and stats
 for i in range(len(names)):
-    if stats[i] < 24:
-        color = 'red'
-    elif stats[i] > 24:
-        color = 'green'
-    else:
-        color = 'orange'
-    print("Character:", names[i], "\nHealth:", colored(stats[i], color), "hp \n")
+    for s in range(6):  # 6 stats per character get color coded
+        stats = stats_tot[i]
+        if stats[s] < 10:
+            color = 'red'
+        elif stats[s] > 10:
+            color = 'green'
+        else:
+            color = 'yellow'
+        stats_string += f'{colored(stats[s], color)} '  # adds the color coded stat to a string
+
+    print("Character:", names[i], "\nStats:", stats_string, "\n")
+    stats_string = ""
