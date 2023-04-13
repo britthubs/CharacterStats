3e8b82e213ece701e289c8d504220c018c4ed0fe
britthubs <britt.van.schoors@gmail.com>
2023-04-13 12:21:30 +0200

    Update comments

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def nameAndClear():  # Ask character name and clear stats list
     names.append(charac)  # add the name of the character to a list for the summary at the end
     stats = []  # makes sure this list is empty again before adding stats to it
     return stats
+
+
 def method():  # method list + input method
     print("Method Menu:")
     print("*" * 11)
@@ -24,7 +26,7 @@ def method():  # method list + input method
     while methodNum not in {"1", "2", "3"}:
         methodNum = input("Please only write the number associated with your preferred method: ")
     if methodNum == "1":
-        three_dsix()  # go to the function for method 3d6
+        three_dsix()  # call the right function according to the chosen method
     elif methodNum == "2":
         three_dsix_drop()
     elif methodNum == "3":
@@ -39,7 +41,7 @@ def three_dsix_drop():  # 3 times 3d6, pick highest
                 rand.randint(1, 6) + rand.randint(1, 6) + rand.randint(1, 6)
                 ]  # 3 times 3d6 for one stat
         stat = max(dice)  # take the highest number from the list
-        stats.append(stat)  # at the stat to a list for this character's stats
+        stats.append(stat)  # add the stat to a list for this character's stats
     stats_tot.append(stats)  # put the list of the stats for this character in a list for the summary at the end
     print("Stats completed! \n")
 
@@ -48,7 +50,7 @@ def three_dsix():  # 3d6 per stat
     stats = nameAndClear()
     for i in range(6):
         stat = rand.randint(1, 6) + rand.randint(1, 6) + rand.randint(1, 6)  # 3d6 for one stat
-        stats.append(stat)  # at the stat to a list for this character's stats
+        stats.append(stat)  # add the stat to a list for this character's stats
     stats_tot.append(stats)  # put the list of the stats for this character in a list for the summary at the end
     print("Stats completed! \n")
 
@@ -61,7 +63,7 @@ def four_dsix_drop():  # 4d6 drop the lowest
         stat = 0
         for number in dice:
             stat += number  # add all the remaining numbers from the list
-        stats.append(stat)  # at the stat to a list for this character's stats
+        stats.append(stat)  # add the stat to a list for this character's stats
     stats_tot.append(stats)  # put the list of the stats for this character in a list for the summary at the end
     print("Stats completed! \n")
 
@@ -69,24 +71,22 @@ def four_dsix_drop():  # 4d6 drop the lowest
 # print title
 print("🎲 Character Stats Generator 🎲")
 print("*" * 31)
-method()  # shows the method menu
+method()  # show the method menu
 
 
 # ask for repeat or not
-
 while True:
     again = input("Roll for new character? \ny or n: ")
     while again != "y" and again != "n":
         again = input("Only write y for 'yes' and n for 'no' \nRoll for new character? \ny or n: ")
     if again == "n":
         break
-    method()  # shows the method menu
-
-os.system('cls' if os.name == 'nt' else 'clear')
-print("These are your characters and their stats:")
-print("*" * 42, "\n")
+    method()  # show the method menu
 
 # print a summary of names and stats
+os.system('cls' if os.name == 'nt' else 'clear')  # clear console
+print("These are your characters and their stats:")
+print("*" * 42, "\n")
 for i in range(len(names)):
     for s in range(6):  # 6 stats per character get color coded
         stats = stats_tot[i]
@@ -96,7 +96,6 @@ for i in range(len(names)):
             color = 'green'
         else:
             color = 'yellow'
-        stats_string += f'{colored(stats[s], color)} '  # adds the color coded stat to a string
-
+        stats_string += f'{colored(stats[s], color)} '  # add the color coded stat to a string
     print("Character:", names[i], "\nStats:", stats_string, "\n")
     stats_string = ""
